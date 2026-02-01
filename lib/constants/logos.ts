5f9441894be5308985f93809cdd844ff0395c703
Matt Hickey <matt.hickey@securiti.ai>
2026-02-01 08:39:59 -0500

    Add NC United logo constants for athlete bios

---
+++ lib/constants/logos.ts
@@ -0,0 +1,6 @@
+/**
+ * NC United logo URLs – single source of truth for bios, cards, profiles.
+ * Update NC_UNITED_LOGO_URL when logo changes.
+ */
+export const NC_UNITED_LOGO_URL =
+  "https://w8v0puzioqkz0xzh.public.blob.vercel-storage.com/logo/CqLaWvzmjRuOdctL8VovY-NC%20United.png"
